d70ee80f578935b0eddd22eb2eb82244ae8e25c0
Anders Andersson <anders.andersson@tretton37.com>
2020-12-07 13:29:30 +0100

    golfing more

--- 05/__main__.py
+++ 05/__main__.py
@@ -1,10 +1,6 @@
 import re
-data = open("data", "r").read().splitlines()
+ids = [int(re.sub(r"[B|R]", "1", re.sub(r"[F|L]", "0", line)), 2) for line in open("data", "r").read().splitlines()]
 
-ids = []
-for line in data:
-  ids.append(int(re.sub(r"[B|R]", "1", re.sub(r"[F|L]", "0", line)), 2))
-  
 for seat in range(sorted(ids)[0], sorted(ids)[-1]):
   if seat not in ids:
     print(sorted(ids)[-1], seat)
