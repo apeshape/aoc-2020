0931a68676e3e9b7d1e6f8fe7317aa5f21fbd245
Anders Andersson <anders.andersson@tretton37.com>
2020-12-07 13:24:21 +0100

    golfing 05

--- 05/__main__.py
+++ 05/__main__.py
@@ -1,38 +1,14 @@
+import re
 data = open("data", "r").read().splitlines()
-
-teststr = "BBFFBBFRLL"
-
-def letterToBin(l):
-  if l == "B" or l == "R": 
-    return "1"
-  if l == "F" or l == "L":
-    return "0"
-  return ""
-
-def getSeat(positionString):
-  positions = list(map(letterToBin, list(positionString)))
-  rowstr = "".join(positions[:-3])
-  colstr = "".join(positions[-3:])
-
-  rowval = int(rowstr, 2)
-  colval = int(colstr, 2)
-  return [rowval, colval]
-
-def getSeatId(row, col):
-  return row * 8 + col
 
 ids = []
 highest = 0
 for line in data:
-  row, col = getSeat(line)
-  sid = getSeatId(row, col)
+  sid = int(re.sub(r"[B|R]", "1", re.sub(r"[F|L]", "0", line)), 2)
   ids.append(sid)
   if sid > highest:
     highest = sid
 
-startTest = 53
-for sid in sorted(ids):
-  if sid != startTest:
-    print('sid', sid - 1)
-    break
-  startTest += 1
+for seat in range(sorted(ids)[0], highest):
+  if seat not in ids:
+    print(highest, seat)
