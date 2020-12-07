2832df153a634c6fe3331019c933f6cb03db574c
Anders Andersson <anders.andersson@tretton37.com>
2020-12-07 15:35:59 +0100

    golfing 06

--- 06/__main__.py
+++ 06/__main__.py
@@ -1,26 +1,7 @@
-data = open("data", "r").read()
+from functools import reduce
 
-groups = data.split("\n\n")
-
-def checkAnswersMatch(testAgainst, answers):
-  if len(answers) == 0:
-    return testAgainst
-
-  newTestAgainst = []
-  for otherAnswer in list(answers[0]):
-    if otherAnswer in list(testAgainst) and otherAnswer not in newTestAgainst:
-      newTestAgainst.append(otherAnswer)
-
-  return checkAnswersMatch(newTestAgainst, answers[1:])
-
-sum = 0
-for group in groups:
+def checkAnswers(acc, group):
   answers = group.split('\n')
+  return acc + len(set(answers[0]).intersection( *answers[1:] ))
 
-  # my crappy recursion
-  matches = checkAnswersMatch(answers[0], answers[1:])
-  # 
-  # matches = set(answers[0]).intersection( *answers[1:] )
-  sum += len(matches)
-
-print("SUM::", sum)
+print(reduce(checkAnswers, open("data", "r").read().split("\n\n"), 0))
