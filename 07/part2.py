cf57b8734a892d0e82ba5b5884226fdd794e5e4d
Anders Andersson <anders.andersson@tretton37.com>
2020-12-07 12:00:54 +0100

    even more golf

--- 07/part2.py
+++ 07/part2.py
@@ -1,27 +1,22 @@
 import re
-data = open("testdata", "r").read().splitlines()
+data = open("data", "r").read().splitlines()
 
-bagtypes = {}
 def get_rule(rulestr):
   if(rulestr != "no other bags."):
     bt = re.search(r"(\w*. \w*.)", rulestr[1:])
     return { bt.group(0).strip(): int(rulestr[:1]) }
 
+def bag_contains(bt, total):
+  for rule in bt:
+    bagtype = list(rule.keys())[0]
+    for _res in range(0, rule[bagtype]):
+      total = bag_contains(bagtypes[bagtype], total + 1)
+  return total
+
+bagtypes = {}
 for line in data:
   bagtype, contents = (lambda x = line.split(" contain "): [x[0].replace(" bags", ""), x[1].split(", ")])()
   rules = [get_rule(rulestr) for rulestr in contents if get_rule(rulestr)]
   bagtypes[bagtype] = rules
 
-def bag_contains(bt, total):
-  for rule in bt:
-    bagtype = list(rule.keys())[0]
-
-    newtotal = [bag_contains(bagtypes[bagtype], total + 1) for rng in range(0, rule[bagtype])]
-    print('newtotal', newtotal)
-    for res in range(0, rule[bagtype]):
-      total = bag_contains(bagtypes[bagtype], total + 1)
-  
-  return total
-
-total = bag_contains(bagtypes["shiny gold"], 0)
-print(total)
+print(bag_contains(bagtypes["shiny gold"], 0))
