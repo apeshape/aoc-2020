0648c17c8ab61dfce225d663c2d0bae04536bd8a
Anders Andersson <anders.andersson@tretton37.com>
2020-12-07 11:54:05 +0100

    more golf

--- 07/part2.py
+++ 07/part2.py
@@ -2,31 +2,21 @@ import re
 data = open("data", "r").read().splitlines()
 
 bagtypes = {}
-
 def get_rule(rulestr):
-  bt = re.search(r"(\w*. \w*.)", rulestr[1:])
-  return {
-    "amount": int(rulestr[:1]),
-    "bagtype": bt.group(0).strip()
-  }
-
+  if(rulestr != "no other bags."):
+    bt = re.search(r"(\w*. \w*.)", rulestr[1:])
+    return { bt.group(0).strip(): int(rulestr[:1]) }
 
 for line in data:
-  bagtype, contains = line.split(" contain ")
-  bagtype = bagtype.replace(" bags", "")
-
-  contents = contains.split(", ")
-  if(contents[0] == "no other bags."):
-    rules = []
-  else:
-    rules = [get_rule(rulestr) for rulestr in contents]
-
+  bagtype, contents = (lambda x = line.split(" contain "): [x[0].replace(" bags", ""), x[1].split(", ")])()
+  rules = [get_rule(rulestr) for rulestr in contents if get_rule(rulestr)]
   bagtypes[bagtype] = rules
 
 def bag_contains(bt, total):
   for rule in bt:
-    for res in range(0, rule["amount"]):
-      total = bag_contains(bagtypes[rule["bagtype"]], total + 1)
+    bagtype = list(rule.keys())[0]
+    for res in range(0, rule[bagtype]):
+      total = bag_contains(bagtypes[bagtype], total + 1)
   
   return total
 
