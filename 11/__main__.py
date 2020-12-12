de2fbd7ff10e084dea01ca4e93ac6bc95a63228b
Anders Andersson <anders.andersson@tretton37.com>
2020-12-12 06:48:25 +0100

    tried optimizing 11

--- 11/__main__.py
+++ 11/__main__.py
@@ -1,41 +1,47 @@
-from time import sleep
+import operator
 from copy import deepcopy
-data = open("data", "r").read().splitlines()
+
+data = open("testdata", "r").read().splitlines()
 seatmap = [list(line) for line in data]
+
 
 def in_bounds(x, y, seatmap):
   return x >= 0 and x < len(seatmap[0]) and y >= 0 and y < len(seatmap)
 
+
 def check_direction(direction, start_pos, seatmap):
-  xdir, ydir = direction
   x, y = start_pos
+  xdir, ydir = direction
+  # x, y = tuple(map(operator.add, direction, start_pos))
   while in_bounds(x + xdir, y + ydir, seatmap):
     x += xdir
     y += ydir
+    # x, y = tuple(map(operator.add, direction, start_pos))
     space = seatmap[y][x]
-    if(space == '#'):
+    if (space == '#'):
       return True
-    if(space == 'L'):
+    if (space == 'L'):
       return False
   return False
 
 
 def get_neighbours(pos, seatmap):
   directions = [
-    [-1,-1],
-    [0,-1],
-    [1,-1],
-    [-1,0],
-    [1,0],
-    [-1,1],
-    [0,1],
-    [1,1]
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1)
   ]
   count = 0
   for direction in directions:
     count += int(check_direction(direction, pos, seatmap))
 
   return count
+
 
 def apply_rules(seatmap):
   new_seatmap = deepcopy(seatmap)
@@ -44,29 +50,20 @@ def apply_rules(seatmap):
     for idx, seat in enumerate(seatline):
       if seat == '.':
         continue
-      neighbours = get_neighbours([idx, idy], seatmap)
-      if neighbours == 0:
+      neighbours = get_neighbours((idx, idy), seatmap)
+      if neighbours == 0 and seat != '#':
         new_seatmap[idy][idx] = '#'
         changed = True
-      elif neighbours >= 5:
+      elif neighbours >= 5 and seat != 'L':
         new_seatmap[idy][idx] = 'L'
         changed = True
   return [changed, new_seatmap]
 
-def get_seatstring(seatmap):
-  lines = []
-  for seatline in seatmap:
-    lines.append(''.join(seatline))
 
-  return ''.join(lines)
-
-seatstring = ''
 while True:
   [changed, seatmap] = apply_rules(seatmap)
-  new_seatstring = get_seatstring(seatmap)
-  if new_seatstring == seatstring:
+  if not changed:
     break
-  seatstring = new_seatstring
 
-
-print('eq', get_seatstring(seatmap).count('#'))
+seatstr = ''.join([''.join(line) for line in seatmap])
+print('eq', seatstr.count('#'))
